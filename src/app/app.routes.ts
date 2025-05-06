318943a4232d49ff8e8430381254c33d8796f6bf
Ashwini810 <ashwinimallesh5036@gmail.com>
2025-05-06 22:22:18 +0530

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'home',pathMatch:'full'},
+    {path:'login',loadComponent:()=>import('./Standalone/Login/login/login.component').then(l=>l.LoginComponent)},
+    {path:'home',component:AppComponent},
+];
